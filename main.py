eeb1bd2593f8544d9f61c86e3f0d4addb7021aec
Nitish Bharti <nitishbharti235@gmail.com>
2020-08-11 16:12:30 +0530

    add main.py for flask app

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request, render_template
+from flask_socketio import SocketIO, emit
+from engineio.payload import Payload
+import json
+
+Payload.max_decode_packets = 500
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode='eventlet')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app)
