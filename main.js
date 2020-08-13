c6877ae62f8a6587aeda2802db10c21a1936feec
Kanupriya Singh <44517226+kanupriyaa@users.noreply.github.com>
2020-08-13 22:25:52 +0530

    Add 2 players
    2 players are added, each player has a paddle that moves with the mouse cursor

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+var player1;
+var player2;
 var b;
 
 function setup() {
     b = new ball();
     createCanvas(windowWidth, windowHeight);
+    player1 = new player(10, height / 2);
+    player2 = new player(width - 30, height / 2);
 }
 
 function draw() {
@@ -10,6 +14,15 @@ function draw() {
     rect(width / 2, 0, 5, windowHeight);
     textSize(30);
     fill(160, 78, 180);
+    
+    text("Player 1 : " + player1.points, 50, 40);
+    text("Player 2 : " + player2.points, width - 300, 40);
+
+    player1.move(mouseY);
+    player2.move(mouseY);
+    player1.show();
+    player2.show();
+
     b.show();
     b.move();
 }
